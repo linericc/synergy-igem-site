d970b7edc43baa33c76e538c85e3ed21a812daf8
linericc <eric.kaoyulin@gmail.com>
2025-12-19 22:31:22 +0800

    debug

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",   // ← IMPORTANT
-});
+  plugins: [react()],
+  base: "/",        // correct for custom domain
+  build: {
+    outDir: "docs", // 👈 IMPORTANT
+  },
+})
